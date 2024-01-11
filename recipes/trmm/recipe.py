32dade0afb4465a7a38b0a76397999fd4b3f97d2
ranchodeluxe <greg.corradini@gmail.com>
2024-01-11 07:31:01 -0800

    add pdb

--- recipes/trmm/recipe.py
+++ recipes/trmm/recipe.py
@@ -220,6 +220,7 @@ class AddBeginDateToZarrGroup(beam.PTransform):
 class AttachBeginDate(beam.DoFn):
     def process(self, element):
         # Assuming each element is a zarr.Group or similar
+        import pdb; pdb.set_trace()
         for key, item in element.items():
             if 'BeginDate' in item.attrs:
                 begin_date = item.attrs['BeginDate']
