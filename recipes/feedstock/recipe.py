0efc5e57f2463b9f8533b5497e7f4d4a5a932b9a
ranchodeluxe <greg.corradini@gmail.com>
2024-03-14 13:30:33 -0700

    try this

--- recipes/feedstock/recipe.py
+++ recipes/feedstock/recipe.py
@@ -8,8 +8,8 @@ from pangeo_forge_recipes.transforms import (
 )
 
 dates = [
-    d.to_pydatetime().strftime('%Y_%m_3b-day.ms.mrg.3imerg.%Y%m%d')
-    for d in pd.date_range('2000-06-01', '2000-09-01', freq='D')
+    d.to_pydatetime().strftime('_%Y_%m_3b-day.ms.mrg.3imerg.%Y%m%d')
+    for d in pd.date_range('2000-06-01', '2005-06-01', freq='D')
 ]
 
 def make_filename(time):
@@ -17,7 +17,6 @@ def make_filename(time):
     # s3://pangeo-forge-uah-test-output/inputs/s3_gesdisc-cumulus-prod-protected_gpm_l3_gpm_3imergdf.07_2001_01_3b-day.ms.mrg.3imerg.20010125-s000000-e235959.v07b.nc4
     base_url = "s3://pangeo-forge-uah-test-output/inputs/s3_gesdisc-cumulus-prod-protected_gpm_l3_gpm_3imergdf.07"
     return f'{base_url}{time}-s000000-e235959.v07b.nc4'
-
 
 concat_dim = ConcatDim('time', dates, nitems_per_file=1)
 pattern = FilePattern(make_filename, concat_dim)
