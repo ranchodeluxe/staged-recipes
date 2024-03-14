9ead54ca70f5b52df44702718bbed72b17f8344d
ranchodeluxe <greg.corradini@gmail.com>
2024-03-14 16:34:37 -0700

    https, 5 years

--- recipes/feedstock/recipe.py
+++ recipes/feedstock/recipe.py
@@ -1,10 +1,3 @@
-# AWS_PROFILE=devseed pangeo-forge-runner bake \
-# --repo=~/Documents/carbonplan/pangeo_forge/staged-recipes/recipes/ \
-# -f ~/Documents/carbonplan/pangeo_forge/staged-recipes/recipes/feedstock/config.py \
-# --Bake.recipe_id=GPM_3IMERGDF.07 \
-# --Bake.job_name=local_test
-
-
 import base64
 import json
 import os
@@ -27,8 +20,8 @@ from pangeo_forge_recipes.transforms import (
 ED_USERNAME = os.environ['EARTHDATA_USERNAME']
 ED_PASSWORD = os.environ['EARTHDATA_PASSWORD']
 
-earthdata_protocol = 's3'
-# earthdata_protocol = 'https'
+#earthdata_protocol = 's3'
+earthdata_protocol = 'https'
 if earthdata_protocol not in ('https', 's3'):
     raise ValueError(f'Unknown ED_PROTOCOL: {earthdata_protocol}')
 
@@ -40,7 +33,7 @@ IDENTICAL_DIMS = ['lat', 'lon']
 # 2023/07/3B-DAY.MS.MRG.3IMERG.20230731
 dates = [
     d.to_pydatetime().strftime('%Y/%m/3B-DAY.MS.MRG.3IMERG.%Y%m%d')
-    for d in pd.date_range('2000-06-01', '2000-12-01', freq='D')
+    for d in pd.date_range('2000-06-01', '2005-06-01', freq='D')
 ]
 #2014-01-01
 
@@ -126,8 +119,6 @@ class DropVarCoord(beam.PTransform):
         # Removing time_bnds since it doesn't have spatial dims
         ds = ds.drop_vars('time_bnds')
         ds = ds[['precipitation']]
-
-        # ds = ds[['precipitation','MWprecipitation','randomError']]
         return index, ds
 
     def expand(self, pcoll: beam.PCollection) -> beam.PCollection:
@@ -161,48 +152,3 @@ recipe = (
         combine_dims=pattern.combine_dim_keys,
     )
 )
-
-
-# ----------------------------------------
-#### LOCAL RUNNING W/O pangeo-forge-runner
-# ----------------------------------------
-
-# import fsspec
-# import zarr
-# from pangeo_forge_recipes.storage import FSSpecTarget
-
-# pipeline = beam.Pipeline()
-# pipeline = beam.Pipeline(runner="DirectRunner", options=beam.pipeline.PipelineOptions(["--num_workers", '2', "--direct_running_mode", "multi_processing"]))
-
-
-# fs = fsspec.get_filesystem_class("file")()
-# path = 'tmp'
-# target_root = FSSpecTarget(fs, path)
-
-# fs = fsspec.get_filesystem_class("s3")()
-# path = 's3://carbonplan-scratch/pyramid'
-# target_root = FSSpecTarget(fs, path)
-
-# import s3fs
-# s3 = s3fs.S3FileSystem()
-# s3.rm(path, recursive=True)
-
-# pattern = pattern.prune()
-# with pipeline as p:
-
-#     (p | beam.Create(pattern.items())
-#     | OpenURLWithFSSpec(open_kwargs=fsspec_open_kwargs)
-#     | OpenWithXarray(file_type=pattern.file_type)
-#     | TransposeCoords()
-#     | DropVarCoord()
-
-#     | 'Write Pyramid Levels'
-#     >> StoreToPyramid(
-#         target_root=target_root,
-#         store_name=SHORT_NAME,
-#         epsg_code='4326',
-#         rename_spatial_dims={'lon': 'longitude', 'lat': 'latitude'},
-#         n_levels=2,
-#         pyramid_kwargs={'extra_dim': 'nv'},
-#         combine_dims=pattern.combine_dim_keys,
-#     ))
