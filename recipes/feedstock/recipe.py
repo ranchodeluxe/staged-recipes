50106fb162cd511eca44a5baf7a7f2adf252563f
ranchodeluxe <greg.corradini@gmail.com>
2024-03-18 04:32:37 -0700

    experiments

--- recipes/feedstock/recipe.py
+++ recipes/feedstock/recipe.py
@@ -1,5 +1,7 @@
-from dataclasses import dataclass
-from typing import Tuple, List, NewType
+import io
+from contextlib import contextmanager
+from dataclasses import dataclass, field
+from typing import Tuple, List, NewType, Optional, Iterator, Union, Any, Dict
 
 import apache_beam as beam
 import pandas as pd
@@ -9,6 +11,7 @@ import zarr
 
 from pangeo_forge_recipes.patterns import ConcatDim, FilePattern
 from pangeo_forge_recipes.types import Index, Dimension
+from pangeo_forge_recipes.writers import ZarrWriterMixin
 from pangeo_forge_recipes.transforms import (
     DetermineSchema,
     IndexItems,
@@ -38,39 +41,8 @@ concat_dim = ConcatDim('time', dates, nitems_per_file=1)
 pattern = FilePattern(make_filename, concat_dim)
 
 
-Url = NewType('Url', str)
-
-@dataclass
-class Example(beam.PTransform):
-    """just the first couple of steps of StoreToZarr to prove a point and singleton reduce
-    """
-    combine_dims: List[Dimension]
-
-    @staticmethod
-    def opener(item) -> xr.Dataset:
-        index, url = item
-        with fsspec.open(url, mode="rb") as open_fs:
-            ds = xr.open_dataset(open_fs, engine='h5netcdf')
-        return index, ds
-
-    def expand(
-        self,
-        urls: beam.PCollection[Tuple[Index, Url]],
-    ) -> beam.PCollection[zarr.storage.FSStore]:
-            datasets = urls | beam.Map(self.opener)
-            schema = datasets | DetermineSchema(combine_dims=self.combine_dims)
-            indexed_datasets = datasets | IndexItems(schema=schema)
-            singleton = (
-                    indexed_datasets
-                    | beam.combiners.Sample.FixedSizeGlobally(1)
-                    | beam.FlatMap(lambda x: x)
-            )
-            return singleton
-
-
 recipe = (
     beam.Create(pattern.items())
-    | Example(
-        combine_dims=pattern.combine_dim_keys,
-    )
+    | FSXRFactory()
+    |
 )
