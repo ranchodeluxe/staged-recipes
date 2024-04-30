2dfa93cd6a253f6bdc7c9cc892633f3ebf8c0312
sudobangbang <gregcorradini@gmail.com>
2024-04-30 05:43:46 -0700

    manually assume role

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -19,22 +19,22 @@ import logging
 logging.getLogger().setLevel(logging.DEBUG)
 
 
-# def assume_role() -> None:
-#     """role chain from EMR execution role to DAACC approved role
+def assume_role() -> None:
+    """role chain from EMR execution role to DAACC approved role
 
-#     :return:
-#     """
-#     client = boto3.client('sts')
-#     creds = client.assume_role(
-#         RoleArn='arn:aws:iam::444055461661:role/veda-data-reader-dev',
-#         RoleSessionName='emr-pforge-runner',
-#         DurationSeconds=3600,
-#     )['Credentials']
-#     return {
-#         "key": creds["AccessKeyId"],
-#         "secret": creds["SecretAccessKey"],
-#         "token": creds["SessionToken"],
-#     }
+    :return:
+    """
+    client = boto3.client('sts')
+    creds = client.assume_role(
+        RoleArn='arn:aws:iam::444055461661:role/veda-data-reader-dev',
+        RoleSessionName='emr-pforge-runner',
+        DurationSeconds=3600,
+    )['Credentials']
+    return {
+        "key": creds["AccessKeyId"],
+        "secret": creds["SecretAccessKey"],
+        "token": creds["SessionToken"],
+    }
 
 
 SHORT_NAME = 'GPM_3IMERGDF.07'
@@ -44,7 +44,7 @@ IDENTICAL_DIMS = ['lat', 'lon']
 # 2023/07/3B-DAY.MS.MRG.3IMERG.20230731
 dates = [
     d.to_pydatetime().strftime('%Y/%m/3B-DAY.MS.MRG.3IMERG.%Y%m%d')
-    for d in pd.date_range('2000-06-01', '2020-06-01', freq='D')
+    for d in pd.date_range('2000-06-01', '2000-07-01', freq='D')
 ]
 
 
@@ -94,7 +94,7 @@ source_fsspec_kwargs = {
   'anon': False,
   'client_kwargs': {'region_name': 'us-west-2'},
 }
-# source_fsspec_kwargs.update(assume_role())
+source_fsspec_kwargs.update(assume_role())
 
 
 target_fsspec_kwargs = {
